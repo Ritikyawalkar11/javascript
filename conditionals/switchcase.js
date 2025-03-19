449f7a19946bdbac89d236ae24b90178210c9cc0
Ritikyawalkar11 <160318036+Ritikyawalkar11@users.noreply.github.com>
2025-03-19 06:51:38 +0000

    conditions

---
+++ conditionals/switchcase.js
@@ -0,0 +1,16 @@
+//using switch case in js
+const month=3
+switch(month){
+    case 1:
+        console.log("the month is janauary")
+        break;
+    case 2:
+        console.log("the month is feb")
+        break;
+    case 3:
+        console.log("the month is march")
+        break;
+    default:
+        console.log("the default text is executed")
+        break;
+}
